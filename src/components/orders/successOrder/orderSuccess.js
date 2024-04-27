f4fe9d9b2397b9d8e0db9dfd9b80a5efe4b1fb67
MOHDSHEES <mohd.shees100@gmail.com>
2024-04-27 18:44:22 +0530

    removed invoice pdf

--- src/components/orders/successOrder/orderSuccess.js
+++ src/components/orders/successOrder/orderSuccess.js
@@ -1,19 +1,19 @@
 import { Alert, Box, Button, CardContent } from "@mui/material";
 import React from "react";
 import { Card } from "react-bootstrap";
-import generatePDF from "../../functions/generatePdf";
-import InvoiceTemplate from "../selectProducts/invoiceTemplate";
+// import generatePDF from "../../functions/generatePdf";
+// import InvoiceTemplate from "../selectProducts/invoiceTemplate";
 
-const handleDownload = async () => {
-  const pdfBlob = await generatePDF();
-  const url = window.URL.createObjectURL(pdfBlob);
-  const link = document.createElement("a");
-  link.href = url;
-  link.setAttribute("download", "Invoice.pdf"); // Set the desired filename
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
+// const handleDownload = async () => {
+//   const pdfBlob = await generatePDF();
+//   const url = window.URL.createObjectURL(pdfBlob);
+//   const link = document.createElement("a");
+//   link.href = url;
+//   link.setAttribute("download", "Invoice.pdf"); // Set the desired filename
+//   document.body.appendChild(link);
+//   link.click();
+//   document.body.removeChild(link);
+// };
 
 const OrderSuccess = ({ invoiceData, handleReset }) => {
   // console.log(invoiceData);
@@ -26,20 +26,20 @@ const OrderSuccess = ({ invoiceData, handleReset }) => {
             <b>{invoiceData.orderId}</b>
           </Alert>
 
-          <Button sx={{ mt: 3 }} onClick={handleDownload} variant="outlined">
+          {/* <Button sx={{ mt: 3 }} onClick={handleDownload} variant="outlined">
             download Invoice
-          </Button>
+          </Button> */}
         </CardContent>
       </Card>
       <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
         <Box sx={{ flex: "1 1 auto" }} />
         <Button onClick={handleReset}>Create More</Button>
       </Box>
-      {invoiceData && (
+      {/* {invoiceData && (
         <Card sx={{ p: 2, mb: 3 }} className="mt-3">
           <InvoiceTemplate invoiceData={invoiceData} />
         </Card>
-      )}
+      )} */}
     </div>
   );
 };
