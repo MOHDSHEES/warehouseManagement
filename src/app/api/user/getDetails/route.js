de7f488fbbb941f354aa05180c029b1bec4c2d35
MOHDSHEES <mohd.shees100@gmail.com>
2024-07-09 04:00:13 +0530

    auto signout err try 1

--- src/app/api/user/getDetails/route.js
+++ src/app/api/user/getDetails/route.js
@@ -7,41 +7,42 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req) {
   const session = await getServerSession(req);
+  console.log(session);
   // Check if the user is authenticated
-  if (session && session.user && session.user.name) {
-    try {
-      await dbConnect();
-      const data = await req.json();
-      if (req.method === "POST") {
-        // const admin = await companyModel
-        //   .findOne({ email: data.email }, { password: 0 })
-        //   .populate("warehouses");
-        const resu = await userModel
-          .findOne({ email: data.email, status: 1 }, { password: 0 })
-          .populate({
-            path: "company",
-            select: { password: 0 },
-          })
-          .populate("warehouse")
-          .populate({ path: "privilegesTemplate", model: privilegesModel });
-        // if (admin) {
-        //   return NextResponse.json({ status: 200, data: admin });
-        // }
-        // console.log(resu);
-        if (resu) {
-          return NextResponse.json({ status: 200, data: resu });
-        } else {
-          return NextResponse.json({
-            status: 500,
-            msg: "Oops! No data found.",
-          });
-        }
+  // if (session && session.user && session.user.name) {
+  try {
+    await dbConnect();
+    const data = await req.json();
+    if (req.method === "POST") {
+      // const admin = await companyModel
+      //   .findOne({ email: data.email }, { password: 0 })
+      //   .populate("warehouses");
+      const resu = await userModel
+        .findOne({ email: data.email, status: 1 }, { password: 0 })
+        .populate({
+          path: "company",
+          select: { password: 0 },
+        })
+        .populate("warehouse")
+        .populate({ path: "privilegesTemplate", model: privilegesModel });
+      // if (admin) {
+      //   return NextResponse.json({ status: 200, data: admin });
+      // }
+      // console.log(resu);
+      if (resu) {
+        return NextResponse.json({ status: 200, data: resu });
       } else {
-        throw new Error(`Unsupported HTTP method: ${req.method}`);
+        return NextResponse.json({
+          status: 500,
+          msg: "Oops! No data found.",
+        });
       }
-    } catch (error) {
-      // console.log(error);
-      return NextResponse.json({ error });
+    } else {
+      throw new Error(`Unsupported HTTP method: ${req.method}`);
     }
-  } else return NextResponse.json({ status: 501, msg: "Not Response" });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error });
+  }
+  // } else return NextResponse.json({ status: 501, msg: "Not Response" });
 }
